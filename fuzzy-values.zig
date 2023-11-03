f1b256ccf0e44a30f1a632a1d0f907f9ed1a52b9
ppdx999 <ppdx999@gmail.com>
2023-11-03 16:31:50 +0900

    improve create randome value implementation

--- fuzzy-values.zig
+++ fuzzy-values.zig
@@ -14,13 +14,52 @@ pub fn main() !void {
     defer freeRandomChars(allocator, array);
 
     try stdout.print("array: {s}\n", .{array});
+
+    const chars2 = "0123456789";
+    var array2 = try setRandomChars(allocator, chars2, 5, 5);
+    defer freeRandomChars(allocator, array2);
+
+    try stdout.print("array2: {s}\n", .{array2});
 }
 
-// fn allocSentinel(allocator: std.mem.Allocator, comptime T: type, count: usize, comptime sentinel: T) ![:sentinel]T {
-//     var ptr = try allocator.alloc(T, count + 1);
-//     ptr[count] = sentinel;
-//     return ptr[0..count :sentinel];
-// }
+fn setRandomCharsv2(allocator: std.mem.Allocator, chars: []const u8, len: u32, count: u32) ![][:0]u8 {
+    // allocate a memory block that is large enough to hold all the strings
+    const contents_bytes = try math.mul(usize, @sizeOf([]u8), count * (len + 1));
+    const slice_list_bytes = try math.mul(usize, @sizeOf(usize), count);
+    const total_bytes = try math.add(usize, contents_bytes, slice_list_bytes);
+    const buf = try allocator.alignedAlloc(u8, @alignOf([]u8), total_bytes);
+
+    // create a slice list that points to the memory block
+    const slice_list = buf[0..slice_list_bytes];
+    var contents_index: usize = 0;
+    for (slice_list, 0..) |_, i| {
+        slice_list[i] = buf[slice_list_bytes + contents_index ..];
+        contents_index += len + 1;
+    }
+
+    // fill the memory block with random strings
+    var rand = prng.init(@intCast(time.milliTimestamp()));
+    for (slice_list) |slice| {
+        var i: u32 = 0;
+        while (i < len) : (i += 1) {
+            const c = chars[rand.random().int(u32) % chars.len];
+            slice[i] = c;
+        }
+        slice[len] = 0;
+    }
+
+    return slice_list;
+}
+
+fn freeRandomCharsv2(allocator: std.mem.Allocator, slice: []const [:0]u8) void {
+    const slice_list_bytes = try math.mul(usize, @sizeOf(usize), slice.len);
+    const contents_bytes = try math.mul(usize, @sizeOf([]u8), slice.len * (slice[0].len + 1));
+    const total_bytes = try math.add(usize, slice_list_bytes, contents_bytes);
+    const buf = slice.ptr - slice_list_bytes;
+    const unaligned_allocated_buf = @as([*]const u8, @ptrCast(buf))[0..total_bytes];
+    const aligned_allocated_buf: []align(@alignOf([]u8)) const u8 = @alignCast(unaligned_allocated_buf);
+    return allocator.free(aligned_allocated_buf);
+}
 
 fn setRandomChars(allocator: std.mem.Allocator, chars: []const u8, len: u32, count: u32) ![][:0]u8 {
     var rand = prng.init(@intCast(time.milliTimestamp()));
